7f1210769d2a67588016600788ca40d6f4c34da3
jlave2 <jlave2@uic.edu>
2015-05-03 01:58:34 -0500

    Major upgrade
    Added user input and possibility for both major/minor scales and many
    keys.

--- irrational.py
+++ irrational.py
@@ -1,29 +1,66 @@
 from math import pi
 import random, winsound
 
-rawFreqs = """  C4    261.63 	131.87
-                C#4/Db4  	277.18 	124.47
-                D4	293.66 	117.48
-                D#4/Eb4  	311.13 	110.89
-                E4	329.63 	104.66
-                F4	349.23 	98.79
-                F#4/Gb4  	369.99 	93.24
-                G4	392.00 	88.01
-                G#4/Ab4  	415.30 	83.07
-                A4	440.00 	78.41
-                A#4/Bb4  	466.16 	74.01
-                B4	493.88 	69.85
-                C5	523.25 	65.93
-                C#5/Db5  	554.37 	62.23
-                D5	587.33 	58.74
-                D#5/Eb5  	622.25 	55.44
-                E5	659.25 	52.33"""
-                
+#-------------------------------------------------------------------------------
+# Read frequency data from the provided text file and store each tone as a list
+# row. The frequency value is converted to an int while storing.
+#-------------------------------------------------------------------------------
+allFreqs = []
+freqFile = open('freqs.txt', 'r')
+for line in freqFile:
+    allFreqs.append([ line.split()[0], int(line.split()[1]) ])
+freqFile.close()
+
+#-------------------------------------------------------------------------------
+# Convert pi into a list of ints, removing the decimal point.
+#-------------------------------------------------------------------------------
+piList = list(str(pi))
+del piList[1]
+piList = [int(digit) for digit in piList]
+
+#-------------------------------------------------------------------------------
+# Define beep durations in milliseconds.
+#-------------------------------------------------------------------------------
 durations = [250, 500, 1000, 2000]
-                
+
 def majorIntervals(n):
     return [n, n+2, n+4, n+5, n+7, n+9, n+11, n+12, n+14, n+16]
+    
+def minorIntervals(n):
+    return [n, n+2, n+3, n+5, n+7, n+8, n+10, n+12, n+14, n+15]    
 
+def getParams():
+    global key, octave, scale
+    key = input('Choose a key (use # or b): ')
+    octave = input('Choose an octave (0-8): ')
+    scale = input('Choose a scale (M/m): ')
+
+def playMusic():
+    getParams()
+    if scale == 'major':
+        toneInds = majorIntervals([allFreqs.index(i) for i in allFreqs \
+                   if key + octave in i[0]][0])
+    if scale == 'minor':
+        toneInds = minorIntervals([allFreqs.index(i) for i in allFreqs \
+                   if key + octave in i[0]][0])
+    else:
+        print('Invalid scale. Try again.')
+        playMusic()
+    if max(toneInds) > len(allFreqs):
+        print('I\'ve run out of tones. Choose a lower octave.')
+        playMusic()
+    else:
+        print('Playing...')
+        freqDict = dict(zip(range(10), toneInds)) 
+        for digit in piList:
+            randDur = random.choice(durations)
+            print('...' + allFreqs[freqDict[digit]][0] + ' (digit ' + \
+                   str(digit) + ') for ' + str(randDur/1000) + ' s')
+            winsound.Beep(allFreqs[freqDict[digit]][1], randDur)
+            
+playMusic()
+
+'''
 allFreqs = map(float, rawFreqs.split()[1::3])
 majorFreqs = [ int(allFreqs[i]) for i in majorIntervals(0) ]
 freqDict = dict(zip(range(10), majorFreqs))
@@ -32,3 +69,4 @@ piList = list(str(pi))
 del piList[1]
 for digit in piList:
     winsound.Beep(freqDict[int(digit)], random.choice(durations))
+    '''
